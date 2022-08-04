127585de50035a358918c4d8a040b2d77dbbf2bb
Andrew Archibald <andrew@aarchibald.com>
2022-08-04 14:59:53 -0600

    Delegate to Range in variant iterator

--- src/syntax/variant.rs
+++ src/syntax/variant.rs
@@ -9,6 +9,7 @@
 use std::fmt::{self, Debug, Formatter};
 use std::iter::FusedIterator;
 use std::marker::PhantomData;
+use std::ops::Range;
 
 pub trait VariantIndex {
     const COUNT: u32;
@@ -20,102 +21,204 @@ pub trait VariantIndex {
     fn index(&self) -> u32;
 
     #[must_use]
-    fn iter() -> VariantIter<Self>
+    #[inline]
+    fn variant_checked(index: u32) -> Option<Self>
     where
         Self: Sized,
     {
-        VariantIter::default()
+        (index < Self::COUNT).then(|| Self::variant(index))
+    }
+
+    #[must_use]
+    #[inline]
+    fn iter() -> VariantRange<Self>
+    where
+        Self: Sized,
+    {
+        VariantRange::default()
     }
 }
 
-pub struct VariantIter<T> {
-    front: u32,
-    back: u32,
+impl const VariantIndex for ! {
+    const COUNT: u32 = 0;
+    #[inline]
+    fn variant(_index: u32) -> Self {
+        unreachable!()
+    }
+    #[inline]
+    fn index(&self) -> u32 {
+        *self
+    }
+}
+
+pub struct VariantRange<T> {
+    range: Range<u32>,
     elem: PhantomData<T>,
 }
 
-impl<T: VariantIndex> Iterator for VariantIter<T> {
+impl<T: VariantIndex> VariantRange<T> {
+    #[inline]
+    pub fn new(start: &T, end: &T) -> Self {
+        VariantRange {
+            range: T::index(&start)..T::index(&end) + 1,
+            elem: PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn contains(&self, variant: &T) -> bool {
+        self.range.contains(&T::index(variant))
+    }
+}
+
+impl<T: VariantIndex> Iterator for VariantRange<T> {
     type Item = T;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.nth(0)
+        self.range.next().map(T::variant)
     }
-
     #[inline]
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        let front = self.front + n as u32;
-        if front < self.back {
-            self.front = front + 1;
-            Some(T::variant(front))
-        } else {
-            self.front = self.back;
-            None
-        }
+        self.range.nth(n).map(T::variant)
     }
-
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let s = (self.back - self.front) as usize;
-        (s, Some(s))
+        self.range.size_hint()
+    }
+    #[inline]
+    fn count(self) -> usize {
+        self.range.count()
+    }
+    #[inline]
+    fn last(self) -> Option<T> {
+        self.range.last().map(T::variant)
+    }
+    #[inline]
+    fn min(self) -> Option<T> {
+        self.range.min().map(T::variant)
+    }
+    #[inline]
+    fn max(self) -> Option<T> {
+        self.range.max().map(T::variant)
     }
 }
 
-impl<T: VariantIndex> DoubleEndedIterator for VariantIter<T> {
+impl<T: VariantIndex> DoubleEndedIterator for VariantRange<T> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.nth_back(0)
+        self.range.next_back().map(T::variant)
     }
-
     #[inline]
     fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-        let n = n as u32;
-        if self.front + n < self.back {
-            self.back = self.back - n - 1;
-            Some(T::variant(self.back))
-        } else {
-            self.back = self.front;
-            None
-        }
+        self.range.nth_back(n).map(T::variant)
     }
 }
 
-impl<T: VariantIndex> ExactSizeIterator for VariantIter<T> {
+impl<T: VariantIndex> ExactSizeIterator for VariantRange<T> {
     #[inline]
     fn len(&self) -> usize {
-        self.size_hint().0
+        self.range.len()
     }
 }
 
-impl<T: VariantIndex> const FusedIterator for VariantIter<T> {}
+impl<T: VariantIndex> const FusedIterator for VariantRange<T> {}
 
-impl<T: VariantIndex> const Default for VariantIter<T> {
+impl<T: VariantIndex> const Default for VariantRange<T> {
     #[inline]
     fn default() -> Self {
-        VariantIter {
-            front: 0,
-            back: T::COUNT,
+        VariantRange {
+            range: 0..T::COUNT,
             elem: PhantomData,
         }
     }
 }
 
-// Avoid extra bounds for T from derive
-impl<T> const Clone for VariantIter<T> {
+impl<T> Clone for VariantRange<T> {
+    #[must_use]
+    #[inline]
     fn clone(&self) -> Self {
-        VariantIter {
-            front: self.front,
-            back: self.back,
+        VariantRange {
+            range: self.range.clone(),
             elem: PhantomData,
         }
     }
 }
-impl<T> const Copy for VariantIter<T> {}
-impl<T> Debug for VariantIter<T> {
+
+impl<T: Debug + VariantIndex> Debug for VariantRange<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("VariantIter")
-            .field("front", &self.front)
-            .field("back", &self.back)
-            .finish()
+        if T::COUNT == 0 {
+            write!(f, "..")
+        } else {
+            let start = T::variant(self.range.start);
+            if self.range.end == 0 || self.range.start >= self.range.end {
+                write!(f, "{:?}", start..T::variant(self.range.end))
+            } else {
+                write!(f, "{:?}", start..=T::variant(self.range.end - 1))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ws::Token;
+
+    macro_rules! check_debug(($left:expr, $right:expr) => {
+        assert_eq!($left, format!("{:?}", $right));
+    });
+
+    #[test]
+    fn iter_no_variants() {
+        let mut iter = <!>::iter();
+        check_debug!("..", iter);
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter_one_variant() {
+        #[allow(dead_code)]
+        #[derive(Debug, PartialEq, Eq)]
+        enum E {
+            V,
+        }
+        impl const VariantIndex for E {
+            const COUNT: u32 = 1;
+            fn variant(_index: u32) -> Self {
+                E::V
+            }
+            fn index(&self) -> u32 {
+                0
+            }
+        }
+
+        let mut iter = E::iter();
+        check_debug!("V..=V", iter);
+        assert_eq!(Some(E::V), iter.next());
+        check_debug!("V..V", iter);
+        iter = E::iter();
+        assert_eq!(Some(E::V), iter.next_back());
+        check_debug!("V..V", iter);
+    }
+
+    #[test]
+    fn iter_three_variants() {
+        let mut iter = Token::iter();
+        check_debug!("S..=L", iter);
+        assert_eq!(Some(Token::S), iter.next());
+        check_debug!("T..=L", iter);
+        assert_eq!(Some(Token::L), iter.next_back());
+        check_debug!("T..=T", iter);
+        assert_eq!(Some(Token::T), iter.next());
+        check_debug!("L..L", iter);
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter_out_of_order() {
+        let mut iter = VariantRange::new(&Token::L, &Token::S);
+        check_debug!("L..T", iter);
+        assert_eq!(None, iter.next());
     }
 }
